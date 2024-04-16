4e384f009cd5925b7a70248e821da51fbe039c6a
Erin Templeton <erindtempleton@gmail.com>
2024-04-16 15:33:17 -0700

    Setup React Router

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,40 @@
-import { BrowswerRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './App.scss';
 
 function App() {
   return (
-    <div className="App">
+<BrowserRouter>
+    {/* Header */}
+      <Routes>
+        {/* reroute to warehouse list?? */}
+        <Route path='/'></Route>
+        
+        {/* Warehouse list */}
+        <Route path='/warehouses'></Route>
 
-    </div>
+        {/* Warehouse add form */}
+        <Route path='/warehouses/add'></Route>
+
+        {/* Warehouse details */}
+        <Route path='/warehouses/:warehouseId'></Route>
+
+        {/* Warehouse edit form */}
+        <Route path='/:warehouseId/:warehouseId/edit'></Route>
+
+        {/* Inventory list */}
+        <Route path='/inventory'></Route>
+        
+        {/* Add inventory item form */}
+        <Route path='/inventory/add'></Route>
+
+        {/* Inventory item details */}
+        <Route path='/inventory/:itemId'></Route>
+
+        {/* Edit inventory item form */}
+        <Route path='/inventory/:itemId/edit'></Route>
+      </Routes>
+    {/* Footer */}
+    </BrowserRouter>
   );
 }
 
