8d334213e95954b2fb0f49e45aec12519fb15218
Erin Templeton <erindtempleton@gmail.com>
2024-04-17 11:25:10 -0700

    Add mobile header component and link functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './App.scss';
+import Header from './components/Header/Header';
 
 function App() {
   return (
 <BrowserRouter>
-    {/* Header */}
+    <Header/>
       <Routes>
         {/* reroute to warehouse list?? */}
         <Route path='/'></Route>
