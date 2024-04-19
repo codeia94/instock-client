30e46d65ce54cc3fc235b396ff81c26338c1cd41
Erin Templeton <erindtempleton@gmail.com>
2024-04-19 12:07:37 -0700

    Finish styling and add dropdown menus

--- src/components/InventoryForm/InventoryForm.jsx
+++ src/components/InventoryForm/InventoryForm.jsx
@@ -1,5 +1,4 @@
 import './InventoryForm.scss';
-import ErrorIcon from '../../assets/icons/error-24px.svg';
 import ButtonPrimary from '../ButtonPrimary/ButtonPrimary';
 import ButtonSecondary from '../ButtonSecondary/ButtonSecondary';
 import { useEffect, useState } from "react";
@@ -15,10 +14,14 @@ export default function InventoryForm () {
     const [ description, setDescription ] = useState("");
     const [ category, setCategory ] = useState("");
     const [ inStock, setInStock ] = useState("Out of stock");
-    const [ quantity, setQuantity ] = useState("");
+    const [ quantity, setQuantity ] = useState("0");
     const [ warehouse, setWarehouse ] = useState("");
     const [ error, setError ] = useState(false);
-    const [ activeInput, setActiveInput ] = useState(null);
+    const [ quantityValid, setQuantityValid ] = useState(true);
+    const [ warehouseMenuVisible, setWarehouseMenuVisible ] = useState(false);
+    const [ warehouseDropdownLable, setWarehouseDropdownLabel ] = useState("Please select");
+    const [ categoryMenuVisible, setCategoryMenuVisible ] = useState(false);
+    const [ categoryDropdownLable, setCategoryDropdownLabel ] = useState("Please select")
 
     // Get warehouse list for dropdown menu
     useEffect(() => {
@@ -37,42 +40,75 @@ export default function InventoryForm () {
     // Change handlers
     const handleChangeItem = (event) => {
         setItem(event.target.value);
-        console.log(item);
     }
     const handleChangeDescription = (event) => {
         setDescription(event.target.value)
-        console.log(description);
     }
     const handleChangeCategory = (event) => {
-        setCategory(event.target.value);
-        console.log(category);
+        setCategory(event.target.id);
+        setCategoryMenuVisible(false);
+        setCategoryDropdownLabel(event.target.id)
     }
     const handleChangeSatus = (event) => {
-        setInStock(event.target.value);
+        setInStock(event.target.value)
+    }
+
+    useEffect(() => {
+        console.log(`inStock set to ${inStock}`);
         if (inStock === "Out of stock") {
             setQuantity("0");
         }
-        console.log(inStock);
-    }
+    }, [inStock]);
+
+    useEffect (() => {
+        const quantityUpdate = isValidQuantity();
+        setQuantityValid(quantityUpdate);
+        console.log(quantityUpdate);
+    }, [quantity, inStock]);
+
     const handleChangeQuantity = (event) => {
         setQuantity(event.target.value);
         console.log(quantity);
     }
     const handleChangeWarehouse = (event) => {
+        console.log(event.target.value)
         setWarehouse(event.target.value);
+        setWarehouseMenuVisible(false);
+        setWarehouseDropdownLabel(event.target.id)
         console.log(warehouse);
     }
-    const handleFocus = (input) => {
-        setActiveInput(input);
+    const handleWarehouseDropdown = () => {
+        if (!warehouseMenuVisible) {
+            setWarehouseMenuVisible(true)
+            setWarehouseDropdownLabel("Please select")
+            setWarehouse("")
+        } else {
+            setWarehouseMenuVisible(false)
+        }
+    }
+
+    const handleCategoryDropdown = () => {
+        if (!categoryMenuVisible) {
+            setCategoryMenuVisible(true)
+            setCategoryDropdownLabel("Please select")
+            setCategory("")
+        } else {
+            setCategoryMenuVisible(false)
+        }
     }
 
     // Validation functions
     const isValidQuantity = () => {
         const num = Number(quantity);
-        if (!num) {
+        console.log(num);
+        if ((inStock === "In stock") && (num <= 0 || !num)) {
+            console.log("validating for state 'In Stock'")
+            setQuantityValid(false);
             return false;
         }
-        if (num < 0) {
+        if ((inStock === "Out of stock") && num > 0) {
+            console.log("validating for state 'Out of Stock'")
+            setQuantityValid(false);
             return false;
         }
         return true;
@@ -84,7 +120,7 @@ export default function InventoryForm () {
             return false;
         }
 
-        if (!isValidQuantity) {
+        if (!quantityValid) {
             setError(true);
             return false;
         }
@@ -93,15 +129,18 @@ export default function InventoryForm () {
     // Submit form
     const handleSubmit = async (event) => {
         event.preventDefault();
-        isFormValid();
-        if (isFormValid) {
+        const valid = isFormValid();
+
+        if (!valid) {
+            return;
+        } else {
             try {
                 console.log("Sending post request...")
                 const requestBody = {
-                    warehouse_id: warehouse,
+                    warehouse_id: Number(warehouse),
                     item_name: item,
                     description: description,
-                    category: category,
+                    category: category.toLocaleLowerCase(),
                     status: inStock,
                     quantity: quantity
                 }
@@ -109,9 +148,7 @@ export default function InventoryForm () {
             } catch (error) {
                 console.log("Error:", error);
             }
-        } else {
-            alert("Failed to add inventory item")
-        }
+    }
     }
 
     return (
@@ -127,11 +164,9 @@ export default function InventoryForm () {
                         value={item}
                         placeholder='Item Name'
                         onChange={handleChangeItem}
-                        onFocus={() => handleFocus('name')}
                         className={
                             `inventory-form__input 
-                            ${error && !item ? "inventory-form__input--error" : ""}
-                            ${activeInput === "name" ? "inventory-form__input--active" : ""}`
+                            ${error && !item ? "inventory-form__input--error" : ""}`
                         }
                     />
                     <FormError errorState={error} field={item}>Item is required</FormError>
@@ -141,36 +176,39 @@ export default function InventoryForm () {
                         name='description'
                         value={description}
                         onChange={handleChangeDescription}
-                        onFocus={() => handleFocus('description')}
                         placeholder='Please enter a brief description...'
                         className={
                             `inventory-form__textarea
-                            ${error && !description ? "inventory-form__textarea--error" : ""}
-                            ${activeInput === "name" ? "inventory-form__textarea--active" : ""}`
+                            ${error && !description ? "inventory-form__textarea--error" : ""}`
                         }
                     />
                     <FormError errorState={error} field={description}>Item is required</FormError>
 
 
                     <label className='inventory-form__label' htmlFor='category'>Category</label>
-                    <select
-                        name='category'
-                        value={category}
-                        onChange={handleChangeCategory}
-                        onFocus={() => handleFocus('category')}
+                    <div
                         className={
-                            `inventory-form__input
-                            ${error && !category ? 'inventory-form__input--error' : ""}
-                            ${activeInput === 'category' ? 'inventory-form__input--active' : ""}`
+                            `inventory-form__input inventory-form__input--dropdown 
+                            ${error && !category ? 'inventory-form__input--error' : ""}`
                         }
                     >
-                        <option value="">Please select</option>
-                        <option value="accessories">Accessories</option>
-                        <option value="apparel">Apparel</option>
-                        <option value="electronics">Electronics</option>
-                        <option value="gear">Gear</option>
-                        <option value="health">Health</option>
-                    </select>
+                        <div 
+                            value=""
+                            className='inventory-form__option'
+                            onClick={handleCategoryDropdown}
+                        >{categoryDropdownLable}</div>
+                        <div className={
+                            `inventory-form__dropdown
+                            ${!categoryMenuVisible ? 'inventory-form__dropdown--hidden' : ""}`
+                            }
+                        >
+                            <div id="Accessories" className='inventory-form__option' onClick={handleChangeCategory}>Accessories</div>
+                            <div id="Apparel" className='inventory-form__option' onClick={handleChangeCategory}>Apparel</div>
+                            <div id="Electronics" className='inventory-form__option' onClick={handleChangeCategory}>Electronics</div>
+                            <div id="Gear" className='inventory-form__option' onClick={handleChangeCategory}>Gear</div>
+                            <div id="Health" className='inventory-form__option' onClick={handleChangeCategory}>Health</div>                    
+                        </div>
+                    </div>
                     <FormError errorState={error} field={category}>Item is required</FormError>
 
                 </div>
@@ -179,72 +217,82 @@ export default function InventoryForm () {
                     <h2 className='inventory-form__subheader'>Item Availability</h2>
 
                     <label className='inventory-form__label' htmlFor='status'>Status</label>
-                    <input
-                        type='radio'
-                        id='in-stock'
-                        name='status'
-                        value="In Stock"
-                        onFocus={() => setActiveInput('in-stock')}
-                        onClick={handleChangeSatus}
-                        />
-                    <label htmlFor='in-stock'>In stock</label>
-                    <input
-                        type='radio'
-                        id='out-of-stock'
-                        name='status'
-                        value="Out of stock"
-                        onFocus={() => setActiveInput('out-of-stock')}
-                        onClick={handleChangeSatus}
-                        defaultChecked
-                    />
-                    <label htmlFor='out-of-stock'>Out of stock</label>
-
-                    <label className='inventory-form__label' htmlFor='quantity'>Quantity</label>
-                    <input
-                        type='text'
-                        name='quantity'
-                        value={quantity}
-                        onChange={handleChangeQuantity}
-                        onFocus={() => setActiveInput('quantity')}
-                        placeholder='0'
+                    <div className='inventory-form__radio-container'>
+                        <div className='inventory-form__radio-option'>
+                            <input
+                                type='radio'
+                                id='in-stock'
+                                name='status'
+                                value="In stock"
+                                onClick={handleChangeSatus}
+                                />
+                            <label htmlFor='in-stock' className='inventory-form__radio'>In stock</label>
+                        </div>
+                        <div className='inventory-form__radio-option'>
+                            <input
+                                type='radio'
+                                id='out-of-stock'
+                                name='status'
+                                value="Out of stock"
+                                onClick={handleChangeSatus}
+                                defaultChecked
+                            />
+                            <label htmlFor='out-of-stock' className='inventory-form__radio'>Out of stock</label>
+                        </div>
+                    </div>
+                    <div 
                         className={
-                            `inventory-form__input
-                            ${inStock === "Out of Stock" ? 'inventory-form__input--hidden' : ""}
-                            ${error && !quantity ? 'inventory-form__input--error' : ""}
-                            ${activeInput === 'quantity' ? 'inventory-form__input--active' : ""}`
-                        }
-                    />
-                    {error && !quantity && (
-                        <span className='inventory-form__error-container'>
-                            <img className='inventory-form__error-icon' src={ErrorIcon} alt='ErrorIcon'></img>
-                            <p className='inventory-form__error-message'>Quantity is required</p>
-                        </span>
-                    )}
+                            `inventory-form__quantity 
+                            ${inStock === "Out of stock" ? 'inventory-form__quantity--hidden' : ""}`
+                        }>
+                        <label className='inventory-form__label' htmlFor='quantity'>Quantity</label>
+                        <input
+                            type='text'
+                            name='quantity'
+                            value={quantity}
+                            onChange={handleChangeQuantity}
+                            placeholder='0'
+                            className={
+                                `inventory-form__input
+                                ${error && !quantity ? 'inventory-form__input--error' : ""}`
+                            }
+                            />
+                    </div>
+                    <FormError errorState={error} field={quantityValid}>Invalid quantity</FormError>
 
                     <label htmlFor='warehouse' className='inventory-form__label'>Warehouse</label>
-                    <select
-                        name='warehouse'
-                        value={warehouse}
-                        onChange={handleChangeWarehouse}
-                        onFocus={() => handleFocus('warehouse')}
+                    <div
                         className={
-                            `inventory-form__input
-                            ${error && !warehouse ? 'inventory-form__input--error' : ""}
-                            ${activeInput === 'warehouse' ? 'inventory-form__input--active' : ""}`
+                            `inventory-form__input inventory-form__input--dropdown 
+                            ${error && !warehouse ? 'inventory-form__input--error' : ""}`
                         }
                     >
-                        <option value="">Please select</option>
-                        {warehouseList.map((warehouse)=>{
-                            return (
-                            <option 
-                                key={warehouse.id} 
-                                value={warehouse.id}
-                            >{warehouse.warehouse_name}
-                            </option>
-                            )})
-                        }
-                    </select>
-                    <FormError errorState={error} field={warehouse}>Item is required</FormError>
+                        <div 
+                            value=""
+                            className='inventory-form__option'
+                            onClick={handleWarehouseDropdown}
+                        >{warehouseDropdownLable}</div>
+                        <div className={
+                            `inventory-form__dropdown
+                            ${!warehouseMenuVisible ? 'inventory-form__dropdown--hidden' : ""}`
+                            }
+                        >
+                            {warehouseList
+                                .map((warehouse)=>{
+                                return (
+                                <div 
+                                    key={warehouse.id} 
+                                    name={warehouse.id}
+                                    id={warehouse.warehouse_name}
+                                    className={'inventory-form__option'}
+                                    onClick={handleChangeWarehouse}
+                                >{warehouse.warehouse_name}
+                                </div>
+                                )})
+                            }
+                        </div>
+                    </div>
+                    <FormError errorState={error} field={warehouse}>Warehouse is required</FormError>
 
                 </div>
             </section>
