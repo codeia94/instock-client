494f6c086908ce7403b02371c4a26b4aa3915806
Leia <leiaooi.94@gmail.com>
2024-04-19 02:20:08 -0700

    Add header and information for warehouse detail page, functional get warehouse by id through url

---
+++ src/pages/WarehouseDetailsPage/WarehouseDetailsPage.jsx
@@ -0,0 +1,54 @@
+import React from "react";
+import WarehouseDetails from '../../components/WarehouseDetails/WarehouseDetails';
+import axios from "axios";
+import './WarehouseDetailsPage.scss';
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import { Link } from "react-router-dom";
+import Edit from '../../assets/icons/edit-24px.svg';
+import BackArrow from '../../assets/icons/arrow_back.svg';
+
+function WarehouseDetailsPage () {
+
+	const { warehouseId } = useParams();
+	const [ warehouseDetails, setWarehouseDetails ] = useState('');
+
+	useEffect(() => {
+		const fetchDetails = async () => {
+
+			try {
+				const response = await axios.get(`http://localhost:8080/api/warehouses/${warehouseId}`);
+				setWarehouseDetails(response.data);
+
+			} catch (error) {
+				console.error(`Error fetching data: ${error}`);
+			}
+		}
+	
+		fetchDetails();
+
+	}, [warehouseId]);
+
+
+	return (
+		<section className='warehouse-details'>
+			<div className='warehouse-details-header'>
+				<div className='warehouse-details-header__nav'>
+					<Link to='/warehouses' className='warehouse-details-header__nav-arrow'>
+						<img src={BackArrow} alt='Back Arrow'/>
+					</Link>
+					<h1 className='warehouse-details-header__nav-current'>{warehouseDetails.warehouse_name}</h1>
+				</div>
+				<button className='warehouse-details-header__btn'>
+					<img className='warehouse-details-header__btn-edit' src={Edit} alt='Edit'/>
+				</button>
+			</div>
+			<hr></hr>
+			<WarehouseDetails details={warehouseDetails}/>
+		</section>
+	)
+}
+
+
+
+export default WarehouseDetailsPage;
