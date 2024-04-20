858db2778fd73becff4fb6bbc0226660de23a58b
Erin Templeton <erindtempleton@gmail.com>
2024-04-20 10:34:16 -0700

    Update useEffect dependencies and error message

--- src/pages/EditInventoryPage/EditInventoryPage.jsx
+++ src/pages/EditInventoryPage/EditInventoryPage.jsx
@@ -1,6 +1,6 @@
 import './EditInventoryPage.scss';
 import axios from "axios";
-import { useParams } from 'react-router-dom';
+import { Link, useParams } from 'react-router-dom';
 import { useState, useEffect } from 'react';
 import InventoryFormHeader from '../../components/InventoryFormHeader/InventoryFormHeader';
 import InventoryForm from '../../components/InventoryForm/InventoryForm';
@@ -10,7 +10,6 @@ export default function EditInventoryPage () {
     const { itemId } = useParams();
 
     const [ data, setData ] = useState("");
-    // const [ warehouseList, setWarehouseList ] = useState([]);
     const [ warehouseId, setWarehouseId ] = useState("");
 
     useEffect(() => {
@@ -19,30 +18,48 @@ export default function EditInventoryPage () {
                 const itemResponse = await axios.get(`http://localhost:8080/api/inventories/${itemId}`);
                 const itemData = itemResponse.data
                 console.log(itemData)
-                setData(itemData)
-                
-                const warehouseResponse = await axios.get(`http://localhost:8080/api/warehouses`);
-                const warehouseList = warehouseResponse.data
-                console.log(warehouseList)
-
-                const itemWarehouse = warehouseList.find((warehouse) => warehouse.warehouse_name === data.warehouse_name)
-                console.log("Warehouse info:", itemWarehouse)
-                setWarehouseId(itemWarehouse.id);
-        
+                setData(itemData)        
             } catch (error) {
-                console.error(`Error fetching data for ID: ${error}`);   
+                console.error(`Error fetching data for ID: ${error}`);  
             }
         }
         getItem();
-    }, []);
+    }, [itemId]);
 
+    
+    useEffect(() => {
+        const getWarehouse = async () => {
+            try {
+                const warehouseResponse = await axios.get(`http://localhost:8080/api/warehouses`);
+                const warehouseList = warehouseResponse.data
+                console.log(warehouseList)
+                
+                const itemWarehouse = warehouseList.find((warehouse) => warehouse.warehouse_name === data.warehouse_name)
+                console.log("Warehouse info:", itemWarehouse)
+                setWarehouseId(itemWarehouse.id);
+            } catch (error) {
+                console.error(`Could not fetch warehouse data.`)
+            }
+        }
+        getWarehouse();
+    }, [data.warehouse_name]);
+    
 
-    if (!data || !warehouseId) {
-        return <>Loading data</>
+    // TODO - how to do the logic below so that it doesn't flash the "Item not found" message while loading the data? Aside from doing another request to look up
+    // whether that id number exists...😭. I can see how this logic is flawed, but I don't know how to fix it...
+
+    if (!data) {
+        return (
+            <div className='error-message'>
+            Inventory item not found.lease return to the&nbsp; <Link to='/inventory'>inventory page</Link>.
+            </div>
+        ) 
+    } else if (!warehouseId) {
+        return <div className='error-message'>Loading data...</div>
     }
 
     return (
-        <div className="add-inventory">
+        <div className="edit-inventory">
             <InventoryFormHeader>Edit New Inventory Item</InventoryFormHeader>
             <InventoryForm
                 data={data}
