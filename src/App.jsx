70304b67b7c77b005ff3d15095de085e9e2cfa94
Leia <leiaooi.94@gmail.com>
2024-09-13 02:42:02 -0700

    add basename for browserrouter

--- src/App.jsx
+++ src/App.jsx
@@ -16,7 +16,7 @@ import Footer from "./components/Footer/Footer";
 
 function App() {
   return (
-<BrowserRouter>
+<BrowserRouter basename='/instock-client'>
     <Header/>
 		<ContentWrapper>
       <Routes>
